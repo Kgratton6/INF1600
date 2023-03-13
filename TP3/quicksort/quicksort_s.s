b0c3af88cdfe12eb5092fa46de6c5e0acccc715c
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2023-03-12 20:41:23 -0400

    parties 1 et 2 termines

--- TP3/quicksort/quicksort_s.s
+++ TP3/quicksort/quicksort_s.s
@@ -19,7 +19,7 @@ jbe retour
 
 movl 12(%ebp), %edx     # Etremite gauche -> i = gauche,
 movl 16(%ebp), %esi
-subl $1, %esi           # nouvelle Etremite droit -> k = droit -1
+subl $1, %esi           # Etremite droit -> k = droit -1
 
 trouver_pivot:          # pivot = medianOfThree(T_, gauche, droit) -> pivot = %eax = T_[droit-1]
 pushl 16(%ebp)
@@ -30,19 +30,46 @@ popl %ecx
 popl %edx
 popl 16(%ebp)
 
-while_i:               # while (T_[++i] < pivot)
-addl $1, %edx
-cmpl %eax, %ecx
+while_i:                # while (T_[++i] < pivot) pivot = %eax, i = %edi
+addl $1, %edx           # i += 1
+
+pushl %ecx 
+pushl %edx
+pushl %eax
+
+movl %edx, %eax        # %ecx = T_[i]
+addl %eax, %ecx
+
+cmp %eax, %ecx         # T_[i] < pivot
+
+popl %eax
+popl %edx
+popl %ecx
+
 jb while_i
 
-while_k:               # while (T_[--k] > pivot) 
-subl $2, %esi                 
-cmpl %eax, %ecx 
+while_k:                # while (T_[--k] > pivot) pivot = %eax, k = %esi
+subl $2, %esi           # k -= 1, car faire des operations desus fais +1
+
+pushl %ecx 
+pushl %edx
+pushl %eax
+
+movl %esi, %eax         # %ecx = T_[k]
+addl %eax, %ecx
+
+cmp %eax, %ecx          # T_[k] > pivot
+
+popl %eax
+popl %edx
+popl %ecx
+
 jb while_k
 
-if:                    # if (i < k) {swapRefs(T_, i, k); mais il faut sauvegarder %eax
-cmpl %esi, %edx
+if:                     # if (i < k) {swapRefs(T_, i, k); mais il faut sauvegarder %eax
+cmp %esi, %edx
 jae changer_place
+
 pushl %eax 
 pushl %esi 
 pushl %edx 
@@ -52,12 +79,11 @@ popl %ecx
 popl %edx
 popl %esi
 popl %eax
+
 jmp while_i
 
 changer_place:          # Permuter T[i] et pivot -> swapRefs (T_, i, droit - 1); 
-movl 16(%ebp), %esi    
-sub $1, %esi            # droite - 1
-pushl %esi        
+movl 16(%ebp), %esi           
 pushl %edx
 pushl %eax  
 push %ecx  
@@ -65,10 +91,10 @@ call swapRefs
 popl %ecx
 popl %eax
 popl %edx
-popl %esi
 
-appel_fonction_gauche:  # Trier les elements sous le pivot -> quicksort_s(T_, gauche, i-1);
-subl $1, %eax           # i - 1
+appel_fonction_gauche:  # Trier les elements sous le pivot
+movl 16(%ebp), %esi
+sub $1, %esi         
 pushl %eax   
 pushl %edx
 pushl %esi
@@ -79,9 +105,8 @@ popl %esi
 popl %edx
 popl %eax
 
-appel_fonction_droite:  # Trier les elements en haut du pivot -> quicksort_s(T_, i+1, droite);
-movl 16(%ebp), %esi     # droite
-addl $1, %eax           # i + 1
+appel_fonction_droite:  # Trier les elements en haut du pivot
+movl 16(%ebp), %esi
 pushl %esi           
 pushl %eax
 pushl %ecx
