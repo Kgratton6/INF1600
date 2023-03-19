39fe43e3c0911e68846ab87126c96f797dc090b5
Thomas <thomas.perron-duveau@polymtl.ca>
2023-03-19 15:55:36 -0400

    Update montantAmasseFinalAvantRetraiteAsm.s

--- TP3/reer/montantAmasseFinalAvantRetraiteAsm.s
+++ TP3/reer/montantAmasseFinalAvantRetraiteAsm.s
@@ -6,9 +6,31 @@ _ZN4Reer34montantAmasseFinalAvantRetraiteAsmEv:
 pushl %ebp
 movl %esp, %ebp
 # DEBUT COMPLETION
+movl 24(%ebp), %ebx     # ebx = taux d'interet
+movl 28(%ebp), %ecx     # ecx = annee de retraite 
+movl 32(%ebp), %edx     # edx = salaire voulu
+movl $100, %esi  
+
+exposant1:
+movl %ebx, %eax
+addl $1, %eax
+subl $1, %ecx
+imul %ebx,%eax               
+loop exposant1
+
+
+
+
+
+
+call _ZN4Reer15salaireFinalAsmEv
+imul %edx, %eax       
+idivl %esi 
 
 # FIN COMPLETION
 # NE RIEN MODIFIER APRES CETTE LIGNE
 retour:
 leave
 ret
+
+
